123fff380a753ba6d4a26bfde5fb31975218ba93
Savan <savan.nagariya@gmail.com>
2021-10-09 13:58:48 +0530

    navbar complete

--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -1,12 +1,30 @@
 import Link from 'next/link';
+import { Flex } from "@chakra-ui/react";
 
-const Navbar = () => {
+
+const Navbar = ({}) => {
     return (
-        <div>
-            <h2>Lists</h2>
-                
-                
-        </div>
+        
+    <Flex className="navbar" justify="space-between"  py={2} px={50}>     
+        <h2>Lists</h2>
+        <Flex alignItems="center" >
+            <div >
+                <Link href="/">
+                <a>Home</a>
+                </Link>
+            </div>
+            <div >
+                <Link href="/about">
+                <a >About Us</a>
+                </Link>
+            </div>
+            <div >
+                <Link href="/list">
+                <a>List</a>
+                </Link>
+            </div>
+        </Flex>
+    </Flex>
     )
 }
 export default Navbar;
